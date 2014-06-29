03fc8d1b58ca8cb36b77437bff87ed0d94d86d81
Swair Shah <swairshah@gmail.com>
2014-06-29 16:16:33 -0500

    support for custom clock in config, default is 1.

--- src/ConfigReader.java
+++ src/ConfigReader.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
  * Created by swair on 6/17/14.
  */
 public class ConfigReader {
+    public static HashMap<Integer,Integer> clocks = new HashMap<>();
     public static HashMap<String,String> getLookup(String f) {
         HashMap<String,String> lookup = new HashMap<String,String>();
         try(BufferedReader r = new BufferedReader(new FileReader(f))) {
@@ -15,7 +16,12 @@ public class ConfigReader {
                 String[] parts = line.split(" ");
                 String pid = parts[0];
                 String ip_port = parts[1]+":"+parts[2];
+                int clock_incr = 1;
+                if(parts.length == 4) {
+                    clock_incr = Integer.parseInt(parts[3]);
+                }
                 lookup.put(pid,ip_port);
+                clocks.put(Integer.parseInt(pid), clock_incr);
             }
         } catch(IOException ex) {
             System.err.println("can't open file");
@@ -24,6 +30,7 @@ public class ConfigReader {
     }
 
     public static void main(String[] args) {
-        //ConfigReader.getLookup("/tmp/config");
+        ConfigReader.getLookup("/tmp/foo");
+        System.out.println(ConfigReader.clocks);
     }
 }
